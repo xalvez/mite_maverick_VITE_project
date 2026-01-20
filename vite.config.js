311d8a8fd41d5bba3af4d56200138507c2e077c0
Majd_Mustafa <xxalvezz@gmail.com>
2026-01-20 11:35:52 -0800

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH ||'/Vite_project_Tababti',
+  base: process.env.VITE_BASE_PATH ||'/mite_maverick_VITE_project',
 })
